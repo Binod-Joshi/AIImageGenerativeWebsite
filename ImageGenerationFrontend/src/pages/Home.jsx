03d138a2ae13fcf2795bf0a3332df9886622bc50
Binod-Joshi <111906023+Binod-Joshi@users.noreply.github.com>
2024-04-06 00:48:19 +0530

    almost completed

--- ImageGenerationFrontend/src/pages/Home.jsx
+++ ImageGenerationFrontend/src/pages/Home.jsx
@@ -15,6 +15,32 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fetch(`http://localhost:5000/api/v1/post`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (response.ok) {
+          const result = await response.json();
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -46,12 +72,9 @@ const Home = () => {
             )}
             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
               {searchText ? (
-                <RenderCards
-                  data={[]}
-                  title="No search results found"
-                />
+                <RenderCards data={[]} title="No search results found" />
               ) : (
-                <RenderCards data={[]} title="No posts found" />
+                <RenderCards data={allPosts} title="No posts found" />
               )}
             </div>
           </>
--- ImageGenerationFrontend/src/utils/index.js
+++ ImageGenerationFrontend/src/utils/index.js
@@ -7,3 +7,7 @@ export function getRandomPrompt(prompt){
     
     return randomPrompt;
 }
+
+export async function downloadImage(_id, photo){
+    FileSaver.saveAs(photo, `download-${_id}.jpg`);
+}
