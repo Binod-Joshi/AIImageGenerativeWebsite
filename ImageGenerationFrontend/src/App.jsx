1a9f12acdee1c01116cb269698bc2adb514ac225
Binod-Joshi <111906023+Binod-Joshi@users.noreply.github.com>
2024-04-09 17:24:03 +0530

    logo changed

--- ImageGenerationFrontend/src/App.jsx
+++ ImageGenerationFrontend/src/App.jsx
@@ -7,12 +7,10 @@ const App = () => {
   return (
     <Router>
       <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
-      <Link to="/" className="flex justify-center items-center gap-5">
-  <img src={logo} alt="logo" className="h-10 w-10 object-contain" />
-  <p className="text-30">Replicate and StabilityAI</p>
-</Link>
-
-
+        <Link to="/" className="flex justify-center items-center gap-5">
+          <img src={logo} alt="logo" className="h-10 w-10 object-contain" />
+          <p className="text-30">Replicate and StabilityAI</p>
+        </Link>
         <Link
           to="/create-post"
           className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
--- ImageGenerationFrontend/src/components/Card.jsx
+++ ImageGenerationFrontend/src/components/Card.jsx
@@ -27,7 +27,11 @@ const Card = ({ _id, name, prompt, photo }) => {
             onClick={() => downloadImage(_id, photo)}
             className="outline-none bg-transparent border-none"
           >
-            <img src={download} alt="download" className="w-6 h-6 object-contain invert"/>
+            <img
+              src={download}
+              alt="download"
+              className="w-6 h-6 object-contain invert"
+            />
           </button>
         </div>
       </div>
